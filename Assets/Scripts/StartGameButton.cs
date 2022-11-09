656cd15a371eeaad08fb8501bba72ea094e01eac
Guimel Paranhos <guimelparanhosdelima24@gmail.com>
2022-11-08 22:56:07 -0300

    Randomização de salas

--- Assets/Scripts/StartGameButton.cs
+++ Assets/Scripts/StartGameButton.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.SceneManagement;
 
-public class ChangeSceneButton : MonoBehaviour
+public class StartGameButton : MonoBehaviour
 {
     [SerializeField] private float threshold = 0.1f;
     [SerializeField] private float deadZone = 0.025f;
@@ -14,6 +14,7 @@ public class ChangeSceneButton : MonoBehaviour
     private bool _isPressed;
     private Vector3 _startPos;
     private ConfigurableJoint _joint;
+    private Vector3 newPlayerPos;
 
     public UnityEvent onPressed, onReleased;
 
@@ -22,6 +23,7 @@ public class ChangeSceneButton : MonoBehaviour
     {
         _startPos = transform.localPosition;
         _joint = GetComponent<ConfigurableJoint>();
+        newPlayerPos.Set(-15, 6, -15);
     }
 
     // Update is called once per frame
@@ -43,11 +45,17 @@ public class ChangeSceneButton : MonoBehaviour
         return Mathf.Clamp(value,-1f,1f);
     }
 
+    private void LoadIt(string scene)
+    {
+        SceneManager.LoadScene(scene, LoadSceneMode.Single);
+        GameObject.Find("Player").transform.position = newPlayerPos;
+    }
+
     private void Pressed()
     {
         _isPressed = true;
         onPressed.Invoke();
-        SceneManager.LoadScene(scene, LoadSceneMode.Single);
+        LoadIt(scene);
         Debug.Log("Pressed");
     }
 
